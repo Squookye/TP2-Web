dfea229e586e1fc465183588fe0a9f682ef54d48
Squookye <alexisrichard7526@gmail.com>
2026-04-21 14:51:50 +0200

    Correction TP8

--- TP8/header.php
+++ TP8/header.php
@@ -1,9 +1,7 @@
 <header>
     <nav>
         <ul>
-            <a href="index.php">Accueil</a>
-            <a href="tp1.php">TP 1</a>
-            <a href="tp2.php">TP 2</a>
+            <a href="index.php">Accueil</a> <p> | </p> <a href="tp1.php">TP 1</a> <p> | </p> <a href="tp2.php">TP 2</a>
         </ul>
     </nav>
 </header>

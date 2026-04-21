6f7c585826704312f9e25fc8f73d06646eee95ae
Squookye <alexisrichard7526@gmail.com>
2026-04-21 14:28:50 +0200

    Correction TP8

--- TP8/header.php
+++ TP8/header.php
@@ -1,6 +1,5 @@
 <header>
     <nav>
-        <a href="index.php"><img src="logo.png" alt="Logo"></a>
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="tp1.php">TP 1</a></li>

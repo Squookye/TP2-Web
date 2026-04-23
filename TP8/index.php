5e6992e631999cf02829ebeaba83ff84fa7a071d
Squookye <alexisrichard7526@gmail.com>
2026-04-23 11:01:06 +0200

    Correction liens

--- TP8/index.php
+++ TP8/index.php
@@ -9,7 +9,20 @@
     
     <main>
         <h1>Bienvenue sur mon portfolio</h1>
-        <p>Ici, tu trouveras tous mes travaux de l'année.</p>
+        <p>Ici, on trouvera tous mes travaux de l'année.</p>
+        <nav>
+        <ul class="nav-list">
+            <li><a class="nav-link" href="https://squookye.github.io/TP1/TP1-Web.pdf">TP 1</a></li>
+            <li><a class="nav-link" href="https://squookye.github.io/TP3/index.html">TP 3</a></li>
+            <li><a class="nav-link" href="https://squookye.github.io/TP4/index.html">TP 4</a></li>
+            <li><a class="nav-link" href="https://squookye.github.io/TP5/index.html">TP 5</a></li>
+            <li><a class="nav-link" href="https://squookye.github.io/TP6/TP6.html">TP 6</a></li>
+            <li><a class="nav-link" href="https://squookye.github.io/TP7/TP7.html">TP 7</a></li>
+            <li><a class="nav-link" href="http://b1.richard.net.local/index.php">TP 8</a></li>
+            <li><a class="nav-link" href="http://b1.richard.net.local/tp9_a.php">TP 9 A</a></li>
+            <li><a class="nav-link" href="http://b1.richard.net.local/tp9_b.php">TP 9 B</a></li>
+        </ul>
+    </nav>
     </main>
 
     <?php include('footer.php'); ?>

58ac6ed24c1fffc427d4eb5ffaed222d666f3217
Squookye <alexisrichard7526@gmail.com>
2026-04-23 11:03:02 +0200

    Ajout TP9 B

--- TP8/header.php
+++ TP8/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul>
-            <p> <a href="index.php">Accueil</a> | <a href="http://b1.richard.net.local/tp9_a.php">TP 9 A</a> </p>
+            <p> <a href="index.php">Accueil</a> | <a href="http://b1.richard.net.local/tp9_a.php">TP 9 A</a> | <a href="http://b1.richard.net.local/tp9_b.php">TP 9 B</a> </p>
         </ul>
     </nav>
 </header>

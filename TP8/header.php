69c434c0f3c3d56e7392b9148003d34d32a731e6
Squookye <alexisrichard7526@gmail.com>
2026-04-23 10:58:29 +0200

    Correction liens TP9

--- TP8/header.php
+++ TP8/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul>
-            <p> <a href="index.php">Accueil</a> | <a href="./TP9/tp9.php">TP 9</a> </p>
+            <p> <a href="index.php">Accueil</a> | <a href="./TP9/tp9_a.php">TP 9</a> </p>
         </ul>
     </nav>
 </header>

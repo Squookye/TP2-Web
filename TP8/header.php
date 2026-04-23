f830c861c5f1227d67be909b9b9a85f57cc4e372
Squookye <alexisrichard7526@gmail.com>
2026-04-23 10:58:01 +0200

    Modification TP8

--- TP8/header.php
+++ TP8/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul>
-            <p> <a href="index.php">Accueil</a> | <a href="tp9.php">TP 9</a> </p>
+            <p> <a href="index.php">Accueil</a> | <a href="./TP9/tp9.php">TP 9</a> </p>
         </ul>
     </nav>
 </header>
